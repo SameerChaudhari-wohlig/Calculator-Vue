c8b0825497d8fe4d5750152297d0ea5b5cbc9703
SameerChaudhari <sameer.chaudhari@wohlig.com>
2022-12-14 18:17:26 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+    <calculator/>
+    </div>
+</template>
+
+<script >
+import Calculator from './Calculator.vue'
+ 
+ export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+ }
+
+</script>
+
+<style>
+ 
+</style>
